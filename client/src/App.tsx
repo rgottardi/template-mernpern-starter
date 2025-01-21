97d6a5cae44e5e9d8b55b4a573dde6bd50b7fc81
rgottardi <62321626+rgottardi@users.noreply.github.com>
2025-01-20 18:18:54 -0700

    Add App component

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h2" component="h1" gutterBottom>
+        Welcome to MERN/PERN Starter
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
